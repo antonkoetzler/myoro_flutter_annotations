f81dadaadd6c1e7c1f13308fe3530049e869fe81
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-05-29 22:07:08 -0300

    initial commit

---
+++ lib/src/model/exports.dart
@@ -0,0 +1,1 @@
+export 'myoro_model_annotation.dart';
---
+++ lib/src/model/myoro_model_annotation.dart
@@ -0,0 +1,7 @@
+/// Annotation class to generate a model.
+final class MyoroModelAnnotation {
+  const MyoroModelAnnotation();
+}
+
+/// The actual annotation.
+const myoroModel = MyoroModelAnnotation();
