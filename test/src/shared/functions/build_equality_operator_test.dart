13c16725b936a83ac086b313da584c9a1c9b07ae
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-06-02 19:47:27 -0300

    saving

---
+++ test/src/shared/functions/build_equality_operator_test.dart
@@ -0,0 +1,50 @@
+import 'package:faker/faker.dart';
+import 'package:myoro_flutter_annotations/src/exports.dart';
+import 'package:test/test.dart';
+
+import '../../../mocks/class_element.mocks.dart';
+import '../../../mocks/field_element.mocks.dart';
+
+void main() {
+  test('buildEqualityOperator empty case', () {
+    final buffer = StringBuffer();
+    final classElement = MockClassElement();
+
+    buildEqualityOperator(buffer, classElement);
+
+    expect(buffer.toString(), '''
+@override
+bool operator ==(Object other) {
+return other is ${classElement.name} &&
+other.runtimeType == runtimeType;
+}
+''');
+  });
+
+  test('buildEqualityOperator non-empty case', () {
+    late final String result;
+
+    final buffer = StringBuffer();
+    final classElement = MockClassElement(
+      fields: List.generate(faker.randomGenerator.integer(10, min: 1), (_) => MockFieldElement()),
+    );
+
+    buildEqualityOperator(buffer, classElement);
+    result = buffer.toString();
+
+    buffer
+      ..clear()
+      ..writeln('@override')
+      ..writeln('bool operator ==(Object other) {')
+      ..writeln('return other is ${classElement.name} &&')
+      ..writeln('other.runtimeType == runtimeType &&');
+    final fieldsLength = classElement.fields.length;
+    for (int i = 0; i < fieldsLength; i++) {
+      final field = classElement.fields[i];
+      buffer.writeln('other.${field.name} == self.${field.name}${(i == fieldsLength - 1) ? ';' : ' &&'}');
+    }
+    buffer.writeln('}');
+
+    expect(result, buffer.toString());
+  });
+}
